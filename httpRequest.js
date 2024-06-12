fcaf62804bc13afea77829ecf45fda85aa545e74
ntwalii <ntwaliaubin@gmail.com>
2024-06-12 18:03:18 +0200

    Promise development

--- httpRequest.js
+++ httpRequest.js
@@ -2,16 +2,19 @@ function getToDo(callback){const request=new XMLHttpRequest();
 
 request.addEventListener('readystatechange',()=>{
     if(request.readyState===4&&request.status===200){
-        callback(undefined,"Done")
+        callback(undefined,JSON.parse(request.responseText))
     }
     else if(request.readyState===4){
         callback("Done but uhhhh",undefined)
     }
 })
 
-request.open('GET','https://jsonplaceholder.typicode.com/todos');
+request.open('GET','chartine.json');
 request.send();
 }
 
-
+console.log(1);
+console.log(2);
 getToDo((err,done)=>err?console.log(err):console.log(done));
+console.log(3);
+console.log(4);
