ad718420b1e11bc218de80e31f51e22e5257c11a
ntwalii <ntwaliaubin@gmail.com>
2024-06-13 14:51:42 +0200

    Promise I m otw

--- httpRequest.js
+++ httpRequest.js
@@ -1,20 +1,28 @@
-function getToDo(callback){const request=new XMLHttpRequest();
+// function getToDo(callback){const request=new XMLHttpRequest();
 
-request.addEventListener('readystatechange',()=>{
-    if(request.readyState===4&&request.status===200){
-        callback(undefined,JSON.parse(request.responseText))
-    }
-    else if(request.readyState===4){
-        callback("Done but uhhhh",undefined)
-    }
-})
+// request.addEventListener('readystatechange',()=>{
 
-request.open('GET','chartine.json');
-request.send();
+//     if(request.readyState===4&&request.status===200){
+//         callback(undefined,JSON.parse(request.responseText))
+//     }
+//     else if(request.readyState===4){
+//         callback("Done but uhhhh",undefined)
+//     }
+// })
+
+// request.open('GET','https://jsonplaceholder.typicode.com/todos');
+// request.send();
+// }
+
+let getSomething=()=>{
+    return new Promise((resolve,reject)=>{
+        // resolve("Passed successfully");
+        reject("Rejected ahaha");
+    })
 }
 
-console.log(1);
-console.log(2);
-getToDo((err,done)=>err?console.log(err):console.log(done));
-console.log(3);
-console.log(4);
+getSomething().then(data=>{
+    console.log(data);
+},data=>{
+    console.error(data)
+})
