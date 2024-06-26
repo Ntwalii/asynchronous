c713dde08f320d5e2fd4635c0fd1554549f40173
ntwalii <ntwaliaubin@gmail.com>
2024-06-26 14:27:46 +0200

    weather api prettier

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,22 +1,26 @@
-async function fetchData(countryName){
-    let latitude;
-    let longitude,capital;
-    try{
-    const response=await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    const countryData=await response.json()
-    let data = countryData[0]
-    latitude=data.latlng[0];
-    longitude=data.latlng[1]; capital=data.capital[0]
-    console.log(`Country: ${countryName}`)
-    console.log(`Capital: ${capital}`)
-    let weatherDetails=await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
-    let weather=await weatherDetails.json()
-    let temperature=weather.current_weather["temperature"];
+async function fetchData(countryName) {
+  let latitude;
+  let longitude, capital;
+  try {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${countryName}`
+    );
+    const countryData = await response.json();
+    let data = countryData[0];
+    latitude = data.latlng[0];
+    longitude = data.latlng[1];
+    capital = data.capital[0];
+    console.log(`Country: ${countryName}`);
+    console.log(`Capital: ${capital}`);
+    let weatherDetails = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+    );
+    let weather = await weatherDetails.json();
+    let temperature = weather.current_weather["temperature"];
     console.log(`Current Temperature: ${temperature}°C`);
-    }
-    catch(error){
-        console.log(error+"Helllo")
-    }
+  } catch (error) {
+    console.log(error + "Helllo");
+  }
 }
 
-fetchData("DRC")
+fetchData("DRC");
