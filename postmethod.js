037e7bea22bbd4dcf006c66d77c9ac58da16f816
ntwalii <ntwaliaubin@gmail.com>
2024-06-27 16:18:21 +0200

    POST method

--- postmethod.js
+++ postmethod.js
@@ -1,13 +1,24 @@
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        title: 'Aubin',
-        body: 'Aubin',
-        userId: 420
-    })
-})
-.then(response => response.json())
-.then(json => console.log(json));
+const postData=async(username,email,password)=>{
+    try{
+        let request=await fetch('https://jsonplaceholder.typicode.com/posts',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            })
+        })
+        if(!request.ok){
+            throw new Error("Failed fetching")
+        }
+        request=await request.json();
+        console.log(request)
+    }
+    catch(error){
+        console.log(response)
+    }
+}
+postData("Ntwalii","user@gmail.com","password")
