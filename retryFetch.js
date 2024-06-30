f3a397a01620ba0f220e8055c49168fb7ec7f6c6
ntwalii <ntwaliaubin@gmail.com>
2024-06-30 20:30:16 +0200

    new weather api

--- retryFetch.js
+++ retryFetch.js
@@ -1,10 +1,21 @@
-const retry=async (url)=>{
-try{
-    const request=await fetch(url);
-    const response=await request.json();
-    console.log(response);
-}
-catch(error){
-    fe
-}
-}
+    const retry = async (url, count) => {
+    try{
+    const response = await fetch(url);
+    if (!response) throw new Error("retrying");
+    const data = await response.json();
+    console.log(data);
+    }
+    catch(error){
+        count--;
+        if(count>0){
+        console.log("Trying");
+        retry('https://jsonplaceholder.typicode.om/todos',count);
+        }
+        else{
+            console.log("Done retrying")
+        }
+
+    }
+    };
+
+    retry("https://jsonplaceholder.typicode.om/todos", 5);
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,26 +1,55 @@
-async function fetchData(countryName) {
-  let latitude;
-  let longitude, capital;
-  try {
-    const response = await fetch(
-      `https://restcountries.com/v3.1/name/${countryName}`
-    );
-    const countryData = await response.json();
-    let data = countryData[0];
-    latitude = data.latlng[0];
-    longitude = data.latlng[1];
-    capital = data.capital[0];
-    console.log(`Country: ${countryName}`);
-    console.log(`Capital: ${capital}`);
-    let weatherDetails = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
-    );
-    let weather = await weatherDetails.json();
-    let temperature = weather.current_weather["temperature"];
-    console.log(`Current Temperature: ${temperature}°C`);
-  } catch (error) {
-    console.log(error + "Helllo");
-  }
+// async function fetchData(countryName) {
+//   let latitude;
+//   let longitude, capital;
+//   try {
+//     const response = await fetch(
+//       `https://restcountries.com/v3.1/name/${countryName}`
+//     );
+//     const countryData = await response.json();
+//     let data = countryData[0];
+//     latitude = data.latlng[0];
+//     longitude = data.latlng[1];
+//     capital = data.capital[0];
+//     console.log(`Country: ${countryName}`);
+//     console.log(`Capital: ${capital}`);
+//     let weatherDetails = await fetch(
+//       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+//     );
+//     let weather = await weatherDetails.json();
+//     let temperature = weather.current_weather["temperature"];
+//     console.log(`Current Temperature: ${temperature}°C`);
+//   } catch (error) {
+//     console.log(error + "Helllo");
+//   }
+// }
+
+// fetchData("DRC");
+
+
+const weatherAPI=async(country)=>{
+    try{
+    const request=await fetch(`https://restcountries.com/v3.1/name/${country}`)
+    
+    const countryDetails=await request.json()
+    
+    const updatedCountryDetails=countryDetails[0]
+    const capital=updatedCountryDetails.capital[0]
+    
+    const latitude=updatedCountryDetails.latlng[0]
+    const longitude=updatedCountryDetails.latlng[1]
+    
+    const weatherFetch=await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+    const weatherDetails=await weatherFetch.json()
+    
+    const temperature=weatherDetails.current_weather.temperature;
+    
+    console.log(`Country : ${country}`)
+    console.log(`Capital : ${capital}`)
+    console.log(`Current Temperature : ${temperature}C`)
+    }
+    catch(error){
+        console.log(`Error faced: ${error}`)
+    }
 }
 
-fetchData("DRC");
+weatherAPI('sudan')
